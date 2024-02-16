e65ee75eecab47fe59ca30682e00e61f5e479d87
Melvin-Shalom <159904270+Melvin-Shalom@users.noreply.github.com>
2024-02-16 23:46:10 +0530

    Create Solution.java

---
+++ Soln_0258/Solution.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Solution {
+  public static int addDigits(int num) {
+    return 1 + (num - 1) % 9;
+  }
+  
+  public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+    System.out.print("Enter the digits: ");
+    int xnum = sc.nextInt();
+    int result = addDigits(xnum);
+    System.out.println("Result: " + result);
+  }
+}
+
+/*
+
+class Solution {
+  public int addDigits(int num) {
+    return 1 + (num - 1) % 9;
+  }
+  
+}*/
