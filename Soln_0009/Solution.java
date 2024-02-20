37570fa7e3d04c4c02e7ca2cb9fa2391500343c7
Melvin-Shalom <okshalommelvin@gmail.com>
2024-02-20 18:00:40 +0530

    106

--- Soln_0009/Solution.java
+++ Soln_0009/Solution.java
@@ -1,2 +1,18 @@
 package Leetcode_Sols.Soln_009;
-// https://github.com/Melvin-Shalom/Leetcode_Sols.git
+
+class Solution {
+    public boolean isPalindrome(int x) {
+        if(x<0)
+            return false;
+
+        long xed = 0;
+        int y = x;
+
+        while (y > 0) {
+            xed = xed * 10 + y % 10;
+            y /= 10;
+        }
+
+        return xed == x;
+    }
+}
